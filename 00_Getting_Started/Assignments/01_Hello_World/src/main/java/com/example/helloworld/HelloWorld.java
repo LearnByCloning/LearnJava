2c0d91ff1ba9bd36c9d74c893d629383ed08a968
Amir Omidi <amir@aaomidi.com>
2016-09-13 03:26:36 -0400

    Added Assignment 1 files

---
+++ 00_Getting_Started/Assignments/01_Hello_World/src/main/java/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        /*
+            Welcome to your first assignment! Exciting!
+
+            You need to figure out how to make this program print hello world!
+         */
+    }
+}
